ffa9924b0b714949da95b4e4c736a21e022e4bf2
123JackCole <123jack.cole@gmail.com>
2020-07-04 11:30:50 -0700

    added add method to linked list class

--- linkedlists.js
+++ linkedlists.js
@@ -12,5 +12,24 @@ class LinkedList {
     constructor() {
         this.head = null;
     }
-    
+
+    add(data) {
+
+        const node = new Node(data);
+        let current;
+
+        if (this.head === null) {
+            this.head = node;
+        } else {
+            current = this.head;
+        }
+
+        while (current.next) {
+            current = current.next;
+        }
+
+        current.next = node;
+
+    }
+
 }
